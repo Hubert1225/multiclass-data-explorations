01742e7cbce947296d17c70fa41ac3b94ed6328c
Hubert1225 <hubibaran@gmail.com>
2023-09-28 15:49:17 +0200

    in load.py: reformat with black

--- scripts/load.py
+++ scripts/load.py
@@ -5,10 +5,10 @@ import numpy as np
 from sklearn.datasets import load_wine
 
 DATASET = sys.argv[1]
-RAW_DATA_DIR = os.path.join('data', 'raw')
+RAW_DATA_DIR = os.path.join("data", "raw")
 
 
-if DATASET == 'wine':
+if DATASET == "wine":
     X, y = load_wine(return_X_y=True, as_frame=True)
-    X.to_csv(os.path.join(RAW_DATA_DIR, 'wine_X.csv'), index=False)
-    np.savetxt(os.path.join(RAW_DATA_DIR, 'wine_y.csv'), y, encoding='utf-8')
+    X.to_csv(os.path.join(RAW_DATA_DIR, "wine_X.csv"), index=False)
+    np.savetxt(os.path.join(RAW_DATA_DIR, "wine_y.csv"), y, encoding="utf-8")
