e0b0bd853a09a876e9b20816c5ba00d303c65938
Hubert1225 <hubibaran@gmail.com>
2023-09-28 14:25:08 +0200

    create script load.py

---
+++ scripts/load.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+import numpy as np
+from sklearn.datasets import load_wine
+
+DATASET = sys.argv[1]
+RAW_DATA_DIR = os.path.join('..', 'data', 'raw')
+
+
+if DATASET == 'wine':
+    X, y = load_wine(return_X_y=True, as_frame=True)
+    X.to_csv(os.path.join(RAW_DATA_DIR, 'wine_X.csv'), index=False)
+    np.savetxt(os.path.join(RAW_DATA_DIR, 'wine_y.csv'), y, encoding='utf-8')
