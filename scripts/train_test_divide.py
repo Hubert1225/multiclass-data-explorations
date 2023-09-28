ab39779c9bcd95f114ac57fd02102a3f31b38010
Hubert1225 <hubibaran@gmail.com>
2023-09-28 23:33:20 +0200

    in train_test_divide.py: reformat with black

--- scripts/train_test_divide.py
+++ scripts/train_test_divide.py
@@ -9,9 +9,9 @@ from params import params
 from randomness import get_random_seed
 
 DATASET = sys.argv[1]
-ALL_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + '.csv')
-TRAIN_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + '_train.csv')
-TEST_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + '_test.csv')
+ALL_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + ".csv")
+TRAIN_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + "_train.csv")
+TEST_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + "_test.csv")
 SEED = get_random_seed(DATASET)
 PARAMS = params[DATASET]
 
@@ -20,7 +20,7 @@ data_train, data_test = train_test_split(
     data_all,
     test_size=PARAMS["test_frac"],
     random_state=SEED,
-    stratify=(data_all['class_'] if PARAMS['stratify'] else None)
+    stratify=(data_all["class_"] if PARAMS["stratify"] else None),
 )
 data_train.to_csv(TRAIN_PATH, index=False)
 data_test.to_csv(TEST_PATH, index=False)
