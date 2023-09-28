b24c5b51473630f2c840473f0c9f7b3813875b7a
Hubert1225 <hubibaran@gmail.com>
2023-09-28 23:32:12 +0200

    in train_test_divide.py: fix parameters reading

--- scripts/train_test_divide.py
+++ scripts/train_test_divide.py
@@ -13,13 +13,14 @@ ALL_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + '.csv')
 TRAIN_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + '_train.csv')
 TEST_PATH = os.path.join(PROCESSED_DATA_DIR, DATASET + '_test.csv')
 SEED = get_random_seed(DATASET)
+PARAMS = params[DATASET]
 
 data_all = pd.read_csv(ALL_PATH)
 data_train, data_test = train_test_split(
     data_all,
-    test_size=params["test_frac"],
+    test_size=PARAMS["test_frac"],
     random_state=SEED,
-    stratify=(data_all['class_'] if params['stratify'] else None)
+    stratify=(data_all['class_'] if PARAMS['stratify'] else None)
 )
 data_train.to_csv(TRAIN_PATH, index=False)
 data_test.to_csv(TEST_PATH, index=False)
