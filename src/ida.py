01946958bde147e7d40721883b6753f1974426a7
Hubert1225 <hubibaran@gmail.com>
2023-09-29 18:09:53 +0200

    in ida.py: add `missing_values_chisquare_test` function

--- src/ida.py
+++ src/ida.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import chisquare
 
 
 def missing_values_percent(column: pd.Series) -> float:
@@ -14,3 +15,34 @@ def missing_values_percent(column: pd.Series) -> float:
 
     """
     return column.isna().sum() * 100.0 / column.size
+
+
+def missing_values_chisquare_test(column: pd.Series, classes: pd.Series) -> float:
+    """Checks if classes distribution among samples
+    where value in given column is missing is the same as
+    classes distribution in entire data.
+
+    The chi-square test is performed, which tests the null hypothesis that
+    the respective classes have the given expected frequencies among missing data,
+    where expected frequencies are frequencies in entire data.
+
+    Args:
+        column: values of column for consecutive samples
+        classes: class labels for consequtive samples
+
+    Returns:
+        float: the pvalue of the chi-square test
+
+    """
+    missing_classes = classes[column.isna()]
+    class_labels = pd.Series(classes.unique())
+    # calculate expected freqs of classes among missing values
+    classes_dist_all = class_labels.map(
+        lambda x: (classes == x).sum() / classes.size
+    ).to_numpy()
+    classes_freq_expected = classes_dist_all * missing_classes.size
+    # calculate observed freqs of classes among missing values
+    classes_freq_missing = class_labels.map(
+        lambda x: (missing_classes == x).sum()
+    ).to_numpy()
+    return chisquare(f_obs=classes_freq_missing, f_exp=classes_freq_expected).pvalue
